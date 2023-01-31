16940ce6997f46753c3b1effb635e4ed8696c7ea
GeorgeKalandadze <kalandadzeg777@gmail.com>
2023-01-31 23:03:26 +0400

    react and laravel first full-stack project

---
+++ react/src/router.jsx
@@ -0,0 +1,71 @@
+import {createBrowserRouter, Navigate} from 'react-router-dom'
+import DefaultLeyout from './components/DefaultLeyout';
+import GuestLayout from './components/GuestLayout';
+import Dashboard from './views/Dashboard';
+import Login from './views/Login';
+import NotFound from './views/NotFound';
+import Signup from './views/Signup';
+import Users from './views/Users';
+import UserForm from "./views/UserForm";
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <DefaultLeyout/>,
+        children: [
+
+            {
+                path: '/',
+                element: <Navigate to="/users"/>
+            },
+
+            {
+                path: '/dashboard',
+                element: <Dashboard/>
+            },
+
+            {
+                path: '/users',
+                element: <Users/>
+            },
+
+            {
+                path: '/users/new',
+                element: <UserForm key="userCreate"/>
+            },
+
+            {
+                path: '/users/:id',
+                element: <UserForm key="userUpdate"/>
+            },
+        ]
+    },
+
+    {
+        path:'/',
+        element: <GuestLayout/>,
+        children:[
+
+            {
+                path: '/login',
+                element: <Login/>
+            },
+
+            {
+                path: '/signup',
+                element: <Signup/>
+            },
+        ]
+    },
+
+
+    {
+        path: '*',
+        element: <NotFound/>
+    }
+
+
+]);
+
+
+export default router
